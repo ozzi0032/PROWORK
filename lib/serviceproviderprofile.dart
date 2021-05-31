9eb64fc4865f3622e8b5cc1ed88927c33e535ba1
ozzi0032 <muzair3284@outlook.com>
2021-05-31 10:48:30 +0500

    custom input field & validations

--- lib/serviceproviderprofile.dart
+++ lib/serviceproviderprofile.dart
@@ -6,7 +6,6 @@ import 'package:PROWORK/utills/exceptions.dart';
 import 'package:PROWORK/utills/sharedPrefs.dart';
 import 'package:PROWORK/viewmodel/user_viewmodel.dart';
 import 'package:PROWORK/widgets/appPrimaryButton.dart';
-import 'package:checkbox_grouped/checkbox_grouped.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
---
+++ lib/widgets/appInputField.dart
@@ -0,0 +1,81 @@
+import 'package:PROWORK/style/appColors.dart';
+import 'package:PROWORK/utills/appConstraints.dart';
+import 'package:flutter/material.dart';
+
+class AppCustomInputField extends StatefulWidget {
+  final double height;
+  final String labelText;
+  final String hintText;
+  final Color labelColor;
+  final String subStr;
+  final bool obscure;
+  final TextEditingController controller;
+  final TextInputType keyboardType;
+  final int maxLines;
+  final bool hasValidation;
+  final Widget prefixIcon;
+  AppCustomInputField(
+      {@required this.labelText,
+      this.hintText = '',
+      this.height = 70,
+      this.labelColor = Colors.white,
+      this.subStr,
+      this.controller,
+      this.obscure = false,
+      this.keyboardType = TextInputType.text,
+      this.maxLines = 1,
+      this.hasValidation = false,
+      this.prefixIcon});
+  @override
+  _AppCustomInputFieldState createState() => _AppCustomInputFieldState();
+}
+
+class _AppCustomInputFieldState extends State<AppCustomInputField> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(5.0),
+      height: widget.height,
+      decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(5.0)),
+      child: TextFormField(
+          controller: widget.controller,
+          obscureText: widget.obscure,
+          keyboardType: widget.keyboardType,
+          cursorHeight: 25.0,
+          cursorColor: AppColors.blueColorGoogle,
+          style: TextStyle(fontSize: 18),
+          textAlignVertical: TextAlignVertical.center,
+          maxLines: widget.maxLines,
+          validator: (val) {
+            return widget.hasValidation
+                ? getValidatorMsg(widget.labelText, val)
+                : null;
+          },
+          decoration: InputDecoration(
+              border: InputBorder.none,
+              hintText: widget.hintText,
+              prefixIcon: widget.prefixIcon)),
+    );
+  }
+
+  getValidatorMsg(String t, String value) {
+    switch (t) {
+      case AppConstants.taskTitleLabel:
+        return value.length < 15
+            ? AppConstants.taskTitleValidationMessage
+            : null;
+        break;
+      case AppConstants.taskDesLabel:
+        return value.length < 40 ? AppConstants.taskDesValidationMessage : null;
+        break;
+      case AppConstants.taskBudgetLabel:
+        return value.length < 1
+            ? AppConstants.taskBudgetValidationMessage
+            : null;
+        break;
+    }
+    return null;
+  }
+}
