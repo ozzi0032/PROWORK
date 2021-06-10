dc9dcadd8494d53525b915ce5eca8507007c0559
ozzi0032 <muzair3284@outlook.com>
2021-06-10 23:42:55 +0500

    get user uploaded tasks module

--- lib/screens/buyer/index.dart
+++ lib/screens/buyer/index.dart
@@ -1,7 +1,9 @@
 import 'package:PROWORK/screens/buyer/addTask.dart';
 import 'package:PROWORK/style/appColors.dart';
+import 'package:PROWORK/viewmodel/task_viewmodel.dart';
 import 'package:PROWORK/widgets/taskCard.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class BuyerHomeFragment extends StatefulWidget {
   @override
@@ -12,20 +14,42 @@ class _BuyerHomeFragmentState extends State<BuyerHomeFragment> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton.extended(
-          backgroundColor: AppColors.blueColorGoogle,
-          onPressed: () {
-            Navigator.of(context)
-                .push(MaterialPageRoute(builder: (context) => AddTask()));
-          },
-          label: Row(
-            children: [
-              Icon(Icons.add),
-              Text("Add Task"),
-            ],
-          )),
-      body: ListView.builder(
-          itemCount: 3, itemBuilder: (context, index) => TaskCard()),
-    );
+        floatingActionButton: FloatingActionButton.extended(
+            backgroundColor: AppColors.blueColorGoogle,
+            onPressed: () {
+              Navigator.of(context)
+                  .push(MaterialPageRoute(builder: (context) => AddTask()));
+            },
+            label: Row(
+              children: [
+                Icon(Icons.add),
+                Text("Add Task"),
+              ],
+            )),
+        body: Consumer<TaskViewModel>(builder: (context, model, child) {
+          return model.isLoading
+              ? Center(
+                  child: CircularProgressIndicator(),
+                )
+              : ListView.builder(
+                  itemCount: model.myTasks.length,
+                  itemBuilder: (context, index) {
+                    if (model.myTasks.length > 0) {
+                      return TaskCard(
+                          userName: model.myTasks[index].employer['Profile']
+                                  ['fname'] +
+                              model.myTasks[index].employer['Profile']['lname'],
+                          imageUrl: model.myTasks[index].employer['Profile']
+                              ['profileUrl'],
+                          taskTitle: model.myTasks[index].title,
+                          taskStatus: model.myTasks[index].status,
+                          createdDate: model.myTasks[index].createdDate);
+                    } else {
+                      return Center(
+                        child: Text('Empty'),
+                      );
+                    }
+                  });
+        }));
   }
 }
